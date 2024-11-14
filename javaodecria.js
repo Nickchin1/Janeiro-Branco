8220c9c3df1aee45963ca6a6d907cdea52c0efd5
Nickchin1 <nickolashenrique2006@gmail.com>
2024-11-13 22:44:21 -0300

    mente queimando

--- javaodecria.js
+++ javaodecria.js
@@ -1,1 +1,10 @@
-teste = document.getElementById("").value;
+function mostrar(){
+    let cmt = document.getElementById("cmt").value;
+    let email = document.getElementById("email").value;
+
+    let nome = document.getElementById("nome").value;
+
+    document.getElementById("com").innerHTML = cmt +nome +email;
+
+    }
+    
